41bbecb5a7aa98ddb3b7a222777dbf091286d3f3
budimanfajarf <budimanfajarf@gmail.com>
2020-07-06 18:58:33 +0700

    latest try sue-vue

---
+++ src/components/Global.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <sui-grid divided="vertically">
+    <sui-divider />
+    <sui-grid-row :columns="2">
+      <sui-grid-column>
+        <Lorem />
+      </sui-grid-column>
+      <sui-grid-column>
+        <Lorem />
+      </sui-grid-column>
+    </sui-grid-row>
+    <sui-grid-row :columns="3">
+      <sui-grid-column>
+        <Lorem />
+      </sui-grid-column>
+      <sui-grid-column>
+        <Lorem />
+      </sui-grid-column>
+      <sui-grid-column>
+        <Lorem />
+      </sui-grid-column>
+    </sui-grid-row>
+  </sui-grid>
+</template>
+
+<script>
+import Lorem from './Lorem.vue'
+
+export default {
+  name: 'Global',
+  components: {
+    Lorem
+  }  
+};
+</script>
